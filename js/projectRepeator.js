47bd3a3e5288db939f1a2485c9c4d5a4dcf46727
Jerrin Thomas <jd0christo1678@gmail.com>
2021-03-13 01:17:18 +0530

    Changed Lab and Blog Css

--- js/projectRepeator.js
+++ js/projectRepeator.js
@@ -1,124 +1,124 @@
 const projectData = [
     {
-        projectName : "Design Patterns",
-        projectType : "Study",
-        imageSrc    : "images/app/dp.jpg",
-        projectLink : "https://github.com/JDChristo/Design-Pattern-in-c-sharp",
-        description : " Git Repo for Design Patterns "
+        projectName: "Design Patterns",
+        projectType: "Study",
+        imageSrc: "images/app/dp.jpg",
+        projectLink: "https://github.com/JDChristo/Design-Pattern-in-c-sharp",
+        description: " Git Repo for Design Patterns "
     },
     {
-        projectName : "Keanu",
-        projectType : "Unity",
-        imageSrc    : "images/app/keanu.png",
-        projectLink : "https://jd-christo.itch.io/keanu",
-        description : " Keanu is a cube lost in unknown space. Help him in his journey through this musical puzzle game"
+        projectName: "Keanu",
+        projectType: "Unity",
+        imageSrc: "images/app/keanu.png",
+        projectLink: "https://jd-christo.itch.io/keanu",
+        description: " Keanu is a cube lost in unknown space. Help him in his journey through this musical puzzle game"
     },
     {
-        projectName : "GEHENNA",
-        projectType : "Unity",
-        imageSrc    : "images/app/geh.png",
-        projectLink : "https://jd-christo.itch.io/",
-        description : "Open World Survival, Worked on - Player Controller - NPC Manager - NPC Crowd System - Crafting System - UI System - Art & Animation"
+        projectName: "GEHENNA",
+        projectType: "Unity",
+        imageSrc: "images/app/geh.png",
+        projectLink: "https://jd-christo.itch.io/",
+        description: "Open World Survival, Worked on - Player Controller - NPC Manager - NPC Crowd System - Crafting System - UI System - Art & Animation"
     },
     {
-        projectName : "Basic 3",
-        projectType : "Unity",
-        imageSrc    : "images/app/tile_2.png",
-        projectLink : "http://jd-christo.itch.io/basic-3",
-        description : " Combine The Tiles And Eat Them, But Look Out For Void. Merge and feed on number blocks and increase your score."
+        projectName: "Basic 3",
+        projectType: "Unity",
+        imageSrc: "images/app/tile_2.png",
+        projectLink: "http://jd-christo.itch.io/basic-3",
+        description: " Combine The Tiles And Eat Them, But Look Out For Void. Merge and feed on number blocks and increase your score."
     },
     {
-        projectName : "Dot in Black and White",
-        projectType : "Unity",
-        imageSrc    : "images/app/Tile.png",
-        projectLink : "https://jd-christo.itch.io/dot",
-        description : " Jump and float your way through dangerous black and white platform in this rhythmic platform game. Prepare for almost impossible challenges."
+        projectName: "Dot in Black and White",
+        projectType: "Unity",
+        imageSrc: "images/app/Tile.png",
+        projectLink: "https://jd-christo.itch.io/dot",
+        description: " Jump and float your way through dangerous black and white platform in this rhythmic platform game. Prepare for almost impossible challenges."
     },
     {
-        projectName : "Missile",
-        projectType : "Unity",
-        imageSrc    : "images/app/icon.jpg",
-        projectLink : "https://jd-christo.itch.io/missile-game",
-        description : " Missile is a clone game of same name - Missile! "
+        projectName: "Missile",
+        projectType: "Unity",
+        imageSrc: "images/app/icon.jpg",
+        projectLink: "https://jd-christo.itch.io/missile-game",
+        description: " Missile is a clone game of same name - Missile! "
     },
     {
-        projectName : "Squashy Traffic",
-        projectType : "Unity",
-        imageSrc    : "images/app/ve.png",
-        projectLink : "https://jd-christo.itch.io/squash",
-        description : " This is a game about managing a Road Junction by controlling the traffic lights. You can use coins to repair damaged traffic lights when they need maintenance. "
+        projectName: "Squashy Traffic",
+        projectType: "Unity",
+        imageSrc: "images/app/ve.png",
+        projectLink: "https://jd-christo.itch.io/squash",
+        description: " This is a game about managing a Road Junction by controlling the traffic lights. You can use coins to repair damaged traffic lights when they need maintenance. "
     },
     {
-        projectName : "Ar Earth Project",
-        projectType : "Unity",
-        imageSrc    : "images/app/ear.png",
-        projectLink : "https://jd-christo.itch.io/ar-earth",
-        description : " Marker based AR Project. Download Target png. "
+        projectName: "Ar Earth Project",
+        projectType: "Unity",
+        imageSrc: "images/app/ear.png",
+        projectLink: "https://jd-christo.itch.io/ar-earth",
+        description: " Marker based AR Project. Download Target png. "
     },
     {
-        projectName : "Simple Chess",
-        projectType : "Unity",
-        imageSrc    : "images/app/Chessboard.png",
-        projectLink : "https://jd-christo.itch.io/simple-chess",
-        description : " Its Chess. "
+        projectName: "Simple Chess",
+        projectType: "Unity",
+        imageSrc: "images/app/Chessboard.png",
+        projectLink: "https://jd-christo.itch.io/simple-chess",
+        description: " Its Chess. "
     },
     {
-        projectName : "Runner",
-        projectType : "Unity",
-        imageSrc    : "images/app/run.png",
-        projectLink : "https://jd-christo.itch.io/runner-demo",
-        description : " A hyper-casual game. Jump to the higher stage  and don't fall. Jump using Mouse Button or Tap for android. The Game in development state. "
+        projectName: "Runner",
+        projectType: "Unity",
+        imageSrc: "images/app/run.png",
+        projectLink: "https://jd-christo.itch.io/runner-demo",
+        description: " A hyper-casual game. Jump to the higher stage  and don't fall. Jump using Mouse Button or Tap for android. The Game in development state. "
     },
     {
-        projectName : "Little Explorer",
-        projectType : "Unity",
-        imageSrc    : "images/app/ex.png",
-        projectLink : "https://jd-christo.itch.io/little-explorer",
-        description : " A small gravity physics game. Control and Explore to collect stars "
+        projectName: "Little Explorer",
+        projectType: "Unity",
+        imageSrc: "images/app/ex.png",
+        projectLink: "https://jd-christo.itch.io/little-explorer",
+        description: " A small gravity physics game. Control and Explore to collect stars "
     }
 ];
 const labData = [
     {
-        projectName : "Lazy Ham Name",
-        projectType : "Game",
-        imageSrc    : "images/portfolio-1.jpg",
-        description : "Follow piggy is on a quest for an amount of fruit beyond any pigs dreams. Lazy Ham is upcoming game inspired from SnakeBird. Lazy Ham is a challenging puzzle game"
+        projectName: "Lazy Ham Name",
+        projectType: "Game",
+        imageSrc: "images/portfolio-1.jpg",
+        description: "Follow piggy is on a quest for an amount of fruit beyond any pigs dreams. Lazy Ham is upcoming game inspired from SnakeBird."
     },
     {
-        projectName : "Kitchen CartName",
-        projectType : "Game",
-        imageSrc    : "images/portfolio-1.jpg",
-        description : "Two player 2d cooking game. Highly inspired by Overcooked, Good Pizza"
+        projectName: "Kitchen CartName",
+        projectType: "Game",
+        imageSrc: "images/portfolio-1.jpg",
+        description: "Two player 2d cooking game. Highly inspired by Overcooked, Good Pizza"
     },
     {
-        projectName : "Squashy Traffic 2",
-        projectType : "Game",
-        imageSrc    : "images/portfolio-1.jpg",
-        description : "Game is a puzzle game of increasing difficulty in which each level is a challenging game of logic and intelligence. To complete the level, you have to rotate the road and create a perfect road way."
+        projectName: "Squashy Traffic 2",
+        projectType: "Game",
+        imageSrc: "images/portfolio-1.jpg",
+        description: "Game is a puzzle game of increasing difficulty in which each level is a challenging game of logic. To complete the level, you have to rotate the road and create a perfect road way."
     },
     {
-        projectName : "Excul",
-        projectType : "Tool",
-        imageSrc    : "images/portfolio-1.jpg",
-        description : "A unity tool to read excel sheet."
+        projectName: "Excul",
+        projectType: "Tool",
+        imageSrc: "images/portfolio-1.jpg",
+        description: "A unity tool to read excel sheet."
     },
     {
-        projectName : "Ping Pong RX",
-        projectType : "Game",
-        imageSrc    : "images/portfolio-1.jpg",
-        description : "Multiplayer Ping Pong game with power ups and extras."
+        projectName: "Ping Pong RX",
+        projectType: "Game",
+        imageSrc: "images/portfolio-1.jpg",
+        description: "Multiplayer Ping Pong game with power ups and extras."
     },
     {
-        projectName : "All UI",
-        projectType : "Game",
-        imageSrc    : "images/portfolio-1.jpg",
-        description : "All UI I created in single game"
+        projectName: "All UI",
+        projectType: "Game",
+        imageSrc: "images/portfolio-1.jpg",
+        description: "All UI I created in single game"
     },
     {
-        projectName : "Helio",
-        projectType : "Game",
-        imageSrc    : "images/portfolio-1.jpg",
-        description : "Helio - multiplayer game"
+        projectName: "Helio",
+        projectType: "Game",
+        imageSrc: "images/portfolio-1.jpg",
+        description: "Helio - multiplayer game"
     }
 ];
 
@@ -132,9 +132,9 @@ document.getElementById("section-works").innerHTML = `
 				</div>
             </div>
             <div class="row">
-                ${projectData.map(function(project){
-                    if(project.projectType === "Unity"){
-                    return`
+                ${projectData.map(function (project) {
+    if (project.projectType === "Unity") {
+        return `
                     <div class="col-md-3 text-center col-padding animate-box">
 					<a target="_blank" href="${project.projectLink}" class="work" style="background-image: url(${project.imageSrc});">
 						<div class="desc">
@@ -145,11 +145,11 @@ document.getElementById("section-works").innerHTML = `
 					</a>
 				    </div>
                     `
-                }
-                else{
-                    return
-                }
-            }).join(' ')}
+    }
+    else {
+        return
+    }
+}).join(' ')}
 				
             </div>
 			<div class="row animate-box">
@@ -163,18 +163,26 @@ document.getElementById("section-works").innerHTML = `
 	</div>s
 `
 document.getElementById("section-labs").innerHTML = `
-    ${labData.map(function(project){
-        return`
-        <div class="col-md-4">
-			<div class="fh5co-blog animate-box">
-				<image class="blog-bg" style="background-image: url(${project.imageSrc});"></image>
-				<div class="blog-text">
-					<h3>${project.projectName}</h3>
-					<span class="posted_on">${project.projectType}</span>
-					<p>${project.description}</p>
-				</div> 
-			</div>
+    ${labData.map(function (project) {
+    return `
+        <div class="col-md-4 animate-box">
+        <div class="post">
+		<div class="title-post">
+		<img src=${project.imageSrc} alt="">
+		</div>
+		<div class="descr">
+        <div class="descr-content">
+		<h1>${project.projectName}</h1>
+        <p>${project.description}</p>
+		<div class="cont-infos">
+		<div class="toolUsed">
+		<span>${project.projectType}</span>
+		</div>
+		</div>
+		</div>
+		</div>
+		</div>
 		</div>
         `
-    }).join(' ')}
+}).join(' ')}
 `
